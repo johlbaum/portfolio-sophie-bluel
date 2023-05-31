517befd71d6cbdc0b321fbe07be013f36199cf98
johlbaum <julien.ohlbaum@gmail.com>
2023-05-31 12:37:58 +0200

    Adding categories

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,3 +1,6 @@
+const categoriesSet = new Set();
+
+//Récupération des données depuis le back-end
 fetch("http://localhost:5678/api/works")
   .then((response) => {
     if (!response.ok) {
@@ -6,18 +9,38 @@ fetch("http://localhost:5678/api/works")
     return response.json();
   })
   .then((data) => {
-    data.map((curr) => {
-      const gallery = document.querySelector(".gallery");
-      const article = document.createElement("figure");
-      const image = document.createElement("img");
-      const figcaption = document.createElement("figcaption");
-      image.src = curr.imageUrl;
-      figcaption.innerText = curr.title;
-      gallery.appendChild(article);
-      article.appendChild(image);
-      article.appendChild(figcaption);
-    });
+    showWorks(data);
+    const categoriesArray = Array.from(categoriesSet);
+    showCategories(categoriesArray);
   })
   .catch((error) => {
     console.error(error);
   });
+
+//Affichage des travaux
+const showWorks = (data) => {
+  data.forEach((work) => {
+    const { imageUrl, title, category } = work;
+    const gallery = document.querySelector(".gallery");
+    const figure = document.createElement("figure");
+    const image = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
+    image.src = imageUrl;
+    figcaption.innerText = title;
+    gallery.appendChild(figure);
+    figure.appendChild(image);
+    figure.appendChild(figcaption);
+
+    categoriesSet.add(category.name);
+  });
+};
+
+//Affichage des catégories
+const showCategories = (categoriesArray) => {
+  categoriesArray.forEach((category) => {
+    const filters = document.querySelector(".filters");
+    const filter = document.createElement("p");
+    filter.innerText = category;
+    filters.appendChild(filter);
+  });
+};
