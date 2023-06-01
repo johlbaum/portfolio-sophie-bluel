57c79f129d3b433c9a0950154f7f67d44b2d7040
johlbaum <julien.ohlbaum@gmail.com>
2023-06-01 20:49:33 +0200

    Appearance of the edit button if the user is logged in

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,65 +1,74 @@
-const categoriesSet = new Set();
+document.addEventListener("DOMContentLoaded", function () {
+  const categoriesSet = new Set();
 
-//Récupération des données depuis le back-end
-fetch("http://localhost:5678/api/works")
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Erreur HTTP " + response.status);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    showWorks(data);
-    const categoriesArray = Array.from(categoriesSet);
-    showCategories(categoriesArray);
-    filterProjectsByCategorie(data);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-
-//Affichage des travaux
-const showWorks = (data) => {
-  data.forEach((work) => {
-    const { imageUrl, title, category } = work;
-    const gallery = document.querySelector(".gallery");
-    const figure = document.createElement("figure");
-    const image = document.createElement("img");
-    const figcaption = document.createElement("figcaption");
-    image.src = imageUrl;
-    figcaption.innerText = title;
-    gallery.appendChild(figure);
-    figure.appendChild(image);
-    figure.appendChild(figcaption);
-
-    categoriesSet.add(category.name);
-  });
-};
-
-//Affichage des catégories
-const showCategories = (categoriesArray) => {
-  categoriesArray.forEach((category) => {
-    const filters = document.querySelector(".filters");
-    const filterElement = document.createElement("p");
-    filterElement.classList.add("filter");
-    filterElement.innerText = category;
-    filters.appendChild(filterElement);
-  });
-};
-
-//Filtrer les projets par catégorie
-const filterProjectsByCategorie = (data) => {
-  const filters = document.querySelectorAll(".filter");
-  let selectedCategory;
-  filters.forEach((filter) => {
-    filter.addEventListener("click", function () {
-      selectedCategory = this.innerText;
-      const filteredProjects = data.filter((project) => {
-        return project.category.name === selectedCategory;
-      });
-
-      document.querySelector(".gallery").innerHTML = "";
-      showWorks(filteredProjects);
+  //Récupération des données depuis le back-end
+  fetch("http://localhost:5678/api/works")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erreur HTTP " + response.status);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      showWorks(data);
+      const categoriesArray = Array.from(categoriesSet);
+      showCategories(categoriesArray);
+      filterProjectsByCategorie(data);
+    })
+    .catch((error) => {
+      console.error(error);
     });
-  });
-};
+
+  //Affichage des travaux
+  const showWorks = (data) => {
+    data.forEach((work) => {
+      const { imageUrl, title, category } = work;
+      const gallery = document.querySelector(".gallery");
+      const figure = document.createElement("figure");
+      const image = document.createElement("img");
+      const figcaption = document.createElement("figcaption");
+      image.src = imageUrl;
+      figcaption.innerText = title;
+      gallery.appendChild(figure);
+      figure.appendChild(image);
+      figure.appendChild(figcaption);
+
+      categoriesSet.add(category.name);
+    });
+  };
+
+  //Affichage des catégories
+  const showCategories = (categoriesArray) => {
+    categoriesArray.forEach((category) => {
+      const filters = document.querySelector(".filters");
+      const filterElement = document.createElement("p");
+      filterElement.classList.add("filter");
+      filterElement.innerText = category;
+      filters.appendChild(filterElement);
+    });
+  };
+
+  //Filtrer les projets par catégorie
+  const filterProjectsByCategorie = (data) => {
+    const filters = document.querySelectorAll(".filter");
+    let selectedCategory;
+    filters.forEach((filter) => {
+      filter.addEventListener("click", function () {
+        selectedCategory = this.innerText;
+        const filteredProjects = data.filter((project) => {
+          return project.category.name === selectedCategory;
+        });
+
+        document.querySelector(".gallery").innerHTML = "";
+        showWorks(filteredProjects);
+      });
+    });
+  };
+
+  //Apparition du bouton modifier si l'utilisateur est logué
+  const editProjectsButton = document.querySelector(".edit-projects");
+  const userIsLoggedIn = localStorage.getItem("loginInformation");
+  if (userIsLoggedIn) {
+    editProjectsButton.classList.add("isLoggedIn");
+  }
+});
